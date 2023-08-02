8dbd8081294ca567f68a5c1548b49c3ad7e2772f
ahmedhossain29 <hossainahmad76@gmail.com>
2023-08-02 21:32:14 +0600

    first commit

---
+++ lib/assignment_1.dart
@@ -0,0 +1,19 @@
+void main(){
+   
+    const int a = 10;
+    const int b = 3;
+
+  int additionResult = a + b;
+  int subtractionResult = a - b;
+  int multiplicationResult = a * b;
+  double divisionResult = a / b; 
+  int modulusResult = a % b;
+
+  print("a=$a,b=$b");
+  print("Addition:$a+$b =$additionResult");
+  print("Subtraction:$a-$b = $subtractionResult");
+  print("Multiplication:$a*$b = $multiplicationResult");
+  print("Division:$a/$b = $divisionResult");
+  print("Modulus:$a%$b = $modulusResult");
+    }
+
